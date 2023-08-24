a18837bcbf4c55555d486dca50973333ffd2a5da
Logan French <Loganfrench04@gmail.com>
2023-08-23 20:28:37 -0400

    solved 6

--- problems/06-add-to-twelve.js
+++ problems/06-add-to-twelve.js
@@ -11,12 +11,25 @@ addToTwelve([1, 11, 4, 7, 6]); // true
 addToTwelve([1, 12, 4, 7, 6]); // false
 addToTwelve([1]); // false
 ***********************************************************************/
-
+// base case - arr.length < 2
+//rec case - arr.length > 2
+//rec step - slice
 
 function addToTwelve(arr) {
-  // Your code here 
+  if (arr.length < 2) {
+    return false
+  }
+  if (arr[0] + arr[1] === 12) {
+    return true
+  }
+  return addToTwelve(arr.slice(1))
 }
 
+addToTwelve([1, 3, 4, 7, 5]); // true
+console.log(addToTwelve([1, 3, 4, 7, 6])); // false
+addToTwelve([1, 11, 4, 7, 6]); // true
+addToTwelve([1, 12, 4, 7, 6]); // false
+addToTwelve([1]); // false
 
 /**************DO NOT MODIFY ANYTHING UNDER THIS LINE*****************/
 try {
